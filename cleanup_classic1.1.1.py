a8f6fe3e595e00abab49c6489499d51292f76d9f
falseywinchnet <87070787+falseywinchnet@users.noreply.github.com>
2022-12-10 13:44:43 -0500

    Update cleanup_classic1.1.1.py
    new ideas bring new concepts. the concepts are valuable. the ideas, rarely so.

--- cleanup_classic1.1.1.py
+++ cleanup_classic1.1.1.py
@@ -296,9 +296,38 @@ def numpyentropycheck(data: numpy.ndarray):
   windows =  numpy.lib.stride_tricks.sliding_window_view(raveled, window_shape = 32)
   raveled[0:windows.shape[0]] = numpy.apply_along_axis(func1d = entropy_numba,axis=1,arr=windows)
   return d  
+
+@numba.jit(cache=True)
+def runs(data: numpy.ndarray,size):
+    output = data.copy()
+    index = 0
+    count = 0
+    run = False
+    for each in range(data.size):
+        if data[each] == 1:
+          if run == False:
+            run = True
+            count = 1
+            index = each
+          else:
+            if run == True:
+              count += 1
+        else:
+          if run == True:
+            if count< size:
+            #erase the run
+              output[index:each-1] = 0
+              count = 0
+            else:
+              count = 0
+              run = False
+    if run == True:
+      #terminated during a run. Is it large enough?
+        if count < size:
+          output[-count:] = 0
+    return output
    
 def denoise(data: numpy.ndarray):
-    
     data= numpy.asarray(data,dtype=float) #correct byte order of array   
     stft_r = stft(data,n_fft=512,window=boxcar) #get complex representation
     stft_vr =  numpy.abs(stft_r) #returns the same as other methods
@@ -306,27 +335,44 @@ def denoise(data: numpy.ndarray):
     window = stft_vr[0:32,:]
     window = numpy.pad(window,((0,0),(2,2)),mode="symmetric")
     e = numpyentropycheck(window.T).T[:,2:-2]
-    entropy = numpy.apply_along_axis(func1d=numpy.max,axis=0,arr=e)
+    entropy = numpy.apply_along_axis(func1d=atd,axis=0,arr=e)
     o = numpy.pad(entropy, entropy.size//2, mode='median')
+
     entropy = moving_average(o,14)[entropy.size//2: -entropy.size//2]
-    factor = numpy.max(entropy)/numpy.average(entropy)
     floor = threshhold(stft_vr)  #use the floor from the boxcar
 
     stft_r = stft(data,n_fft=512,window=hann) #get complex representation
     stft_vr =  numpy.abs(stft_r) #returns the same as other methods
 
     stft_vr=(stft_vr-numpy.nanmin(stft_vr))/numpy.ptp(stft_vr) #normalize to 0,1
-    residue = man(stft_vr)   
+    residue = man(stft_vr) 
 
     entropy[entropy<0.0841470984807] = 0
     entropy[entropy>0] = 1
+
+    entropy = runs(entropy, 6) #remove fragments
     nbins = numpy.sum(entropy)
     #14 = ~37ms. For a reliable speech squelch which ignores ionosound chirps, set to ~80-100 bins
     #factor is an unknown, as the method for calculating it is not fully reliable.
-    if nbins < 14 or factor < 1.50:
+    if nbins < 14:
       stft_r = stft_r * residue #return early, and terminate the noise
       processed = istft(stft_r,window=hann)
       return processed 
+
+    threshold = (threshhold(stft_vr[stft_vr>=floor]) - atd(stft_vr[stft_vr>=floor]))
+    mask_two = numpy.where(stft_vr>=threshold, 1.0,0)
+
+    mask = mask_two * entropy[None,:] #remove regions from the mask that are noise
+    residue = residue 
+    mask[mask==0] = residue 
+    mask = numpyfilter_wrapper_50(mask)
+    
+    mask=(mask-numpy.nanmin(mask))/numpy.ptp(mask)#correct basis    
+
+    stft_r = stft_r * mask
+    processed = istft(stft_r,window=hann)
+    return processed
+
 
     threshold = (threshhold(stft_vr[stft_vr>=floor]) - atd(stft_vr[stft_vr>=floor]))
     mask_two = numpy.where(stft_vr>=threshold, 1.0,0)
