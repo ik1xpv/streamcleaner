9e6777d2b54633ceae9236ddea2bd1624ec8639e
falseywinchnet <87070787+falseywinchnet@users.noreply.github.com>
2022-12-10 14:20:51 -0500

    Update demo_fileIO.py

--- demo_fileIO.py
+++ demo_fileIO.py
@@ -48,9 +48,7 @@ from librosa import stft, istft
 from scipy.io import wavfile
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
-
-
-def hann(M, sym=True):
+def fast_hamming(M, sym=True):
     a = [0.5, 0.5]
     fac = numpy.linspace(-numpy.pi, numpy.pi, M)
     w = numpy.zeros(M)
@@ -58,29 +56,52 @@ def hann(M, sym=True):
         w += a[k] * numpy.cos(k * fac)
     return w
 
-def boxcar(M, sym=True):
-    a = [0.5, 0.5]
-    fac = numpy.linspace(-numpy.pi, numpy.pi, M)
-    w = numpy.zeros(M)
-    for k in range(len(a)):
-        w += a[k] * numpy.cos(k * fac)
-        return w
+#@numba.jit()
+#def boxcar(M, sym=True):
+#    a = [0.5, 0.5]
+#    fac = numpy.linspace(-numpy.pi, numpy.pi, M)
+#    w = numpy.zeros(M)
+#    for k in range(len(a)):
+#        w += a[k] * numpy.cos(k * fac)
+#        return w
+boxcar = numpy.asarray([0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5])
 
-def moving_average(x, w):
-    return numpy.convolve(x, numpy.ones(w), 'same') / w
+#@numba.jit()
+#def hann(M, sym=True):
+#    a = [0.5, 0.5]
+#    fac = numpy.linspace(-numpy.pi, numpy.pi, M)
+#    w = numpy.zeros(M)
+#    for k in range(len(a)):
+#        w += a[k] * numpy.cos(k * fac)
+#    return w
+
+hann = numpy.asarray([0.00000000e+00,3.77965773e-05,1.51180595e-04,3.40134910e-04,6.04630957e-04,9.44628746e-04,1.36007687e-03,1.85091253e-03,2.41706151e-03,3.05843822e-03,3.77494569e-03,4.56647559e-03,5.43290826e-03,6.37411270e-03,7.38994662e-03,8.48025644e-03,9.64487731e-03,1.08836332e-02,1.21963367e-02,1.35827895e-02,1.50427819e-02,1.65760932e-02,1.81824916e-02,1.98617342e-02,2.16135671e-02,2.34377255e-02,2.53339336e-02,2.73019047e-02,2.93413412e-02,3.14519350e-02,3.36333667e-02,3.58853068e-02,3.82074146e-02,4.05993391e-02,4.30607187e-02,4.55911813e-02,4.81903443e-02,5.08578147e-02,5.35931893e-02,5.63960544e-02,5.92659864e-02,6.22025514e-02,6.52053053e-02,6.82737943e-02,7.14075543e-02,7.46061116e-02,7.78689827e-02,8.11956742e-02,8.45856832e-02,8.80384971e-02,9.15535940e-02,9.51304424e-02,9.87685015e-02,1.02467221e-01,1.06226043e-01,1.10044397e-01,1.13921708e-01,1.17857388e-01,1.21850843e-01,1.25901469e-01,1.30008654e-01,1.34171776e-01,1.38390206e-01,1.42663307e-01,1.46990432e-01,1.51370928e-01,1.55804131e-01,1.60289372e-01,1.64825973e-01,1.69413247e-01,1.74050502e-01,1.78737036e-01,1.83472140e-01,1.88255099e-01,1.93085190e-01,1.97961681e-01,2.02883837e-01,2.07850913e-01,2.12862158e-01,2.17916814e-01,2.23014117e-01,2.28153297e-01,2.33333576e-01,2.38554171e-01,2.43814294e-01,2.49113148e-01,2.54449933e-01,2.59823842e-01,2.65234062e-01,2.70679775e-01,2.76160159e-01,2.81674384e-01,2.87221617e-01,2.92801019e-01,2.98411747e-01,3.04052952e-01,3.09723782e-01,3.15423378e-01,3.21150881e-01,3.26905422e-01,3.32686134e-01,3.38492141e-01,3.44322565e-01,3.50176526e-01,3.56053138e-01,3.61951513e-01,3.67870760e-01,3.73809982e-01,3.79768282e-01,3.85744760e-01,3.91738511e-01,3.97748631e-01,4.03774209e-01,4.09814335e-01,4.15868096e-01,4.21934577e-01,4.28012860e-01,4.34102027e-01,4.40201156e-01,4.46309327e-01,4.52425614e-01,4.58549094e-01,4.64678841e-01,4.70813928e-01,4.76953428e-01,4.83096412e-01,4.89241951e-01,4.95389117e-01,5.01536980e-01,5.07684611e-01,5.13831080e-01,5.19975458e-01,5.26116815e-01,5.32254225e-01,5.38386758e-01,5.44513487e-01,5.50633486e-01,5.56745831e-01,5.62849596e-01,5.68943859e-01,5.75027699e-01,5.81100196e-01,5.87160431e-01,5.93207489e-01,5.99240456e-01,6.05258418e-01,6.11260467e-01,6.17245695e-01,6.23213197e-01,6.29162070e-01,6.35091417e-01,6.41000339e-01,6.46887944e-01,6.52753341e-01,6.58595644e-01,6.64413970e-01,6.70207439e-01,6.75975174e-01,6.81716305e-01,6.87429962e-01,6.93115283e-01,6.98771407e-01,7.04397480e-01,7.09992651e-01,7.15556073e-01,7.21086907e-01,7.26584315e-01,7.32047467e-01,7.37475536e-01,7.42867702e-01,7.48223150e-01,7.53541070e-01,7.58820659e-01,7.64061117e-01,7.69261652e-01,7.74421479e-01,7.79539817e-01,7.84615893e-01,7.89648938e-01,7.94638193e-01,7.99582902e-01,8.04482319e-01,8.09335702e-01,8.14142317e-01,8.18901439e-01,8.23612347e-01,8.28274329e-01,8.32886681e-01,8.37448705e-01,8.41959711e-01,8.46419017e-01,8.50825950e-01,8.55179843e-01,8.59480037e-01,8.63725883e-01,8.67916738e-01,8.72051970e-01,8.76130952e-01,8.80153069e-01,8.84117711e-01,8.88024281e-01,8.91872186e-01,8.95660845e-01,8.99389686e-01,9.03058145e-01,9.06665667e-01,9.10211707e-01,9.13695728e-01,9.17117204e-01,9.20475618e-01,9.23770461e-01,9.27001237e-01,9.30167455e-01,9.33268638e-01,9.36304317e-01,9.39274033e-01,9.42177336e-01,9.45013788e-01,9.47782960e-01,9.50484434e-01,9.53117800e-01,9.55682662e-01,9.58178630e-01,9.60605328e-01,9.62962389e-01,9.65249456e-01,9.67466184e-01,9.69612237e-01,9.71687291e-01,9.73691033e-01,9.75623159e-01,9.77483377e-01,9.79271407e-01,9.80986977e-01,9.82629829e-01,9.84199713e-01,9.85696393e-01,9.87119643e-01,9.88469246e-01,9.89745000e-01,9.90946711e-01,9.92074198e-01,9.93127290e-01,9.94105827e-01,9.95009663e-01,9.95838660e-01,9.96592693e-01,9.97271648e-01,9.97875422e-01,9.98403924e-01,9.98857075e-01,9.99234805e-01,9.99537058e-01,9.99763787e-01,9.99914959e-01,9.99990551e-01,9.99990551e-01,9.99914959e-01,9.99763787e-01,9.99537058e-01,9.99234805e-01,9.98857075e-01,9.98403924e-01,9.97875422e-01,9.97271648e-01,9.96592693e-01,9.95838660e-01,9.95009663e-01,9.94105827e-01,9.93127290e-01,9.92074198e-01,9.90946711e-01,9.89745000e-01,9.88469246e-01,9.87119643e-01,9.85696393e-01,9.84199713e-01,9.82629829e-01,9.80986977e-01,9.79271407e-01,9.77483377e-01,9.75623159e-01,9.73691033e-01,9.71687291e-01,9.69612237e-01,9.67466184e-01,9.65249456e-01,9.62962389e-01,9.60605328e-01,9.58178630e-01,9.55682662e-01,9.53117800e-01,9.50484434e-01,9.47782960e-01,9.45013788e-01,9.42177336e-01,9.39274033e-01,9.36304317e-01,9.33268638e-01,9.30167455e-01,9.27001237e-01,9.23770461e-01,9.20475618e-01,9.17117204e-01,9.13695728e-01,9.10211707e-01,9.06665667e-01,9.03058145e-01,8.99389686e-01,8.95660845e-01,8.91872186e-01,8.88024281e-01,8.84117711e-01,8.80153069e-01,8.76130952e-01,8.72051970e-01,8.67916738e-01,8.63725883e-01,8.59480037e-01,8.55179843e-01,8.50825950e-01,8.46419017e-01,8.41959711e-01,8.37448705e-01,8.32886681e-01,8.28274329e-01,8.23612347e-01,8.18901439e-01,8.14142317e-01,8.09335702e-01,8.04482319e-01,7.99582902e-01,7.94638193e-01,7.89648938e-01,7.84615893e-01,7.79539817e-01,7.74421479e-01,7.69261652e-01,7.64061117e-01,7.58820659e-01,7.53541070e-01,7.48223150e-01,7.42867702e-01,7.37475536e-01,7.32047467e-01,7.26584315e-01,7.21086907e-01,7.15556073e-01,7.09992651e-01,7.04397480e-01,6.98771407e-01,6.93115283e-01,6.87429962e-01,6.81716305e-01,6.75975174e-01,6.70207439e-01,6.64413970e-01,6.58595644e-01,6.52753341e-01,6.46887944e-01,6.41000339e-01,6.35091417e-01,6.29162070e-01,6.23213197e-01,6.17245695e-01,6.11260467e-01,6.05258418e-01,5.99240456e-01,5.93207489e-01,5.87160431e-01,5.81100196e-01,5.75027699e-01,5.68943859e-01,5.62849596e-01,5.56745831e-01,5.50633486e-01,5.44513487e-01,5.38386758e-01,5.32254225e-01,5.26116815e-01,5.19975458e-01,5.13831080e-01,5.07684611e-01,5.01536980e-01,4.95389117e-01,4.89241951e-01,4.83096412e-01,4.76953428e-01,4.70813928e-01,4.64678841e-01,4.58549094e-01,4.52425614e-01,4.46309327e-01,4.40201156e-01,4.34102027e-01,4.28012860e-01,4.21934577e-01,4.15868096e-01,4.09814335e-01,4.03774209e-01,3.97748631e-01,3.91738511e-01,3.85744760e-01,3.79768282e-01,3.73809982e-01,3.67870760e-01,3.61951513e-01,3.56053138e-01,3.50176526e-01,3.44322565e-01,3.38492141e-01,3.32686134e-01,3.26905422e-01,3.21150881e-01,3.15423378e-01,3.09723782e-01,3.04052952e-01,2.98411747e-01,2.92801019e-01,2.87221617e-01,2.81674384e-01,2.76160159e-01,2.70679775e-01,2.65234062e-01,2.59823842e-01,2.54449933e-01,2.49113148e-01,2.43814294e-01,2.38554171e-01,2.33333576e-01,2.28153297e-01,2.23014117e-01,2.17916814e-01,2.12862158e-01,2.07850913e-01,2.02883837e-01,1.97961681e-01,1.93085190e-01,1.88255099e-01,1.83472140e-01,1.78737036e-01,1.74050502e-01,1.69413247e-01,1.64825973e-01,1.60289372e-01,1.55804131e-01,1.51370928e-01,1.46990432e-01,1.42663307e-01,1.38390206e-01,1.34171776e-01,1.30008654e-01,1.25901469e-01,1.21850843e-01,1.17857388e-01,1.13921708e-01,1.10044397e-01,1.06226043e-01,1.02467221e-01,9.87685015e-02,9.51304424e-02,9.15535940e-02,8.80384971e-02,8.45856832e-02,8.11956742e-02,7.78689827e-02,7.46061116e-02,7.14075543e-02,6.82737943e-02,6.52053053e-02,6.22025514e-02,5.92659864e-02,5.63960544e-02,5.35931893e-02,5.08578147e-02,4.81903443e-02,4.55911813e-02,4.30607187e-02,4.05993391e-02,3.82074146e-02,3.58853068e-02,3.36333667e-02,3.14519350e-02,2.93413412e-02,2.73019047e-02,2.53339336e-02,2.34377255e-02,2.16135671e-02,1.98617342e-02,1.81824916e-02,1.65760932e-02,1.50427819e-02,1.35827895e-02,1.21963367e-02,1.08836332e-02,9.64487731e-03,8.48025644e-03,7.38994662e-03,6.37411270e-03,5.43290826e-03,4.56647559e-03,3.77494569e-03,3.05843822e-03,2.41706151e-03,1.85091253e-03,1.36007687e-03,9.44628746e-04,6.04630957e-04,3.40134910e-04,1.51180595e-04,3.77965773e-05,0.00000000e+00])
 
 
+@numba.jit()
 def man(arr):
-
     med = numpy.nanmedian(arr[numpy.nonzero(arr)])
     return numpy.nanmedian(numpy.abs(arr - med))
 
+@numba.jit()
 def atd(arr):
-    x = numpy.square(abs(arr - man(arr)))
+    x = numpy.square(numpy.abs(arr - man(arr)))
     return numpy.sqrt(numpy.nanmean(x))
+
+@numba.jit()
+def atd1(arr):
+    x = numpy.square(numpy.abs(arr - man(arr)))
+    return numpy.sqrt(numpy.nanmean(x))
+
+@numba.jit()
+def threshhold(arr):
+  return (atd(arr)+ numpy.nanmedian(arr[numpy.nonzero(arr)])) 
+
+
 
 def threshhold(arr):
   return (atd(arr)+ numpy.nanmedian(arr[numpy.nonzero(arr)])) 
+
 
 def corrected_logit(size):
     fprint = numpy.linspace(0, 1, size)
@@ -92,7 +113,7 @@ def corrected_logit(size):
 
 #precalculate the logistic function for our entropy calculations.
 #save some cycles with redundancy.
-#change this value if you change your entropy window.
+#since we only calculate entropy over a fixed window, we can store our logit.
 logit = corrected_logit(32)
 
 def entropy(data: numpy.ndarray):
@@ -103,7 +124,9 @@ def entropy(data: numpy.ndarray):
     sigma = 1 - completeness
     return sigma
 
-    
+def moving_average(x, w):
+    return numpy.convolve(x, numpy.ones(w), 'same') / w
+
 def runningMeanFast(x, N):
     return numpy.convolve(x, numpy.ones((N,))/N,mode="valid")
 
@@ -178,11 +201,7 @@ def denoise_old(data: numpy.ndarray):
     
     trend = moving_average(maxent,20)
     factor = numpy.max(trend)
-    if factor < 0.0577215664: #unknown the exact most precise, correct option. 
-    #this step does a "false alert" for a frame which is either containing some signal or no signal.
-    #generally, anything which is above 0.067 is pretty much guaranteed to contain signal. Anything below 0.55 is guaranteed to be noise.
-    #based on my calculations, 0.56 passes 10% of noise, 0.057 passes 1%. But these settings also pass 100% of signal.
-    #0.058 begins to miss a few of the hardest to read voice segments, normally illegible.
+    if factor < 0.0577215664: 
 
       stft_r = stft_r * r
       processed = istft(stft_r,window=hann)
@@ -213,6 +232,7 @@ def denoise_old(data: numpy.ndarray):
     processed = istft(stft_r,window=hann)
     return processed
 
+
 @numba.jit()
 def entropy_numba(data: numpy.ndarray):
     a = numpy.sort(data)
@@ -221,7 +241,6 @@ def entropy_numba(data: numpy.ndarray):
     completeness = z[0, 1]
     sigma = 1 - completeness
     return sigma
-
 
 def numpyentropycheck(data: numpy.ndarray):
   d = data.copy()
@@ -260,6 +279,7 @@ def runs(data: numpy.ndarray,size):
           output[-count:] = 0
     return output
    
+    
 def denoise(data: numpy.ndarray):
     data= numpy.asarray(data,dtype=float) #correct byte order of array   
     stft_r = stft(data,n_fft=512,window=boxcar) #get complex representation
@@ -268,19 +288,19 @@ def denoise(data: numpy.ndarray):
     window = stft_vr[0:32,:]
     window = numpy.pad(window,((0,0),(2,2)),mode="symmetric")
     e = numpyentropycheck(window.T).T[:,2:-2]
-    entropy = numpy.apply_along_axis(func1d=atd,axis=0,arr=e)
+    entropy = numpy.apply_along_axis(func1d=atd1,axis=0,arr=e)
     o = numpy.pad(entropy, entropy.size//2, mode='median')
 
     entropy = moving_average(o,14)[entropy.size//2: -entropy.size//2]
-    floor = threshhold(stft_vr)  #use the floor from the boxcar
+    floor = threshhold(numpy.ravel(stft_vr))  #use the floor from the boxcar
 
     stft_r = stft(data,n_fft=512,window=hann) #get complex representation
     stft_vr =  numpy.abs(stft_r) #returns the same as other methods
 
     stft_vr=(stft_vr-numpy.nanmin(stft_vr))/numpy.ptp(stft_vr) #normalize to 0,1
-    residue = man(stft_vr) 
+    residue = man(numpy.ravel(stft_vr)) 
 
-    entropy[entropy<0.0841470984807] = 0
+    entropy[entropy<0.078] = 0
     entropy[entropy>0] = 1
 
     entropy = runs(entropy, 6) #remove fragments
@@ -292,11 +312,12 @@ def denoise(data: numpy.ndarray):
       processed = istft(stft_r,window=hann)
       return processed 
 
-    threshold = (threshhold(stft_vr[stft_vr>=floor]) - atd(stft_vr[stft_vr>=floor]))
+
+    threshold = (threshhold(numpy.ravel(stft_vr[stft_vr>=floor])) - atd(numpy.ravel(stft_vr[stft_vr>=floor])))
     mask_two = numpy.where(stft_vr>=threshold, 1.0,0)
 
+
     mask = mask_two * entropy[None,:] #remove regions from the mask that are noise
-    residue = residue 
     mask[mask==0] = residue 
     mask = numpyfilter_wrapper_50(mask)
     
@@ -306,21 +327,6 @@ def denoise(data: numpy.ndarray):
     processed = istft(stft_r,window=hann)
     return processed
 
-
-    threshold = (threshhold(stft_vr[stft_vr>=floor]) - atd(stft_vr[stft_vr>=floor]))
-    mask_two = numpy.where(stft_vr>=threshold, 1.0,0)
-
-
-    mask = mask_two * entropy[None,:] #remove regions from the mask that are noise
-    residue = residue * factor
-    mask[mask==0] = residue 
-    mask = numpyfilter_wrapper_50(mask)
-    
-    mask=(mask-numpy.nanmin(mask))/numpy.ptp(mask)#correct basis    
-
-    stft_r = stft_r * mask
-    processed = istft(stft_r,window=hann)
-    return processed
 
 
 def padarray(A, size):
@@ -332,10 +338,9 @@ def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
-
 def process_data(data: numpy.ndarray):
     print("processing ", data.size / rate, " seconds long file at ", rate, " rate.")
-    start = time()
+    start = time.time()
     processed = []
     for each in chunks(data, rate):
         if each.size == rate:
@@ -345,9 +350,9 @@ def process_data(data: numpy.ndarray):
             working = padarray(each, rate)
             working = denoise(working)
             processed.append(working[0:psize])
-    end = time()
+    end = time.time()
     print("took ", end - start, " to process ", data.size / rate)
-    return numpy.concatenate((processed), axis=0)   
+    return numpy.concatenate((processed), axis=0) 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
