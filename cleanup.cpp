7c7b0db2ee7359758fb27cc4f54bffe4b2c4fc54
falseywinchnet <87070787+falseywinchnet@users.noreply.github.com>
2023-02-21 06:30:44 -0500

    Update cleanup.cpp

--- cleanup.cpp
+++ cleanup.cpp
@@ -22,8 +22,6 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110 - 1301, USA.
 */
-//please note: this project is still a work in process and is not finished.
-//please do not attempt to use this code for any purpose until this line is removed.
 
 
 /*
